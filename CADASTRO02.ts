797271a2a528d20ead965770a753d07918602703
Alex978076 <joseantoniofilipealexandreviei@gmail.com>
2026-05-07 08:47:24 -0300

    Update CADASTRO02.ts

--- CADASTRO02.ts
+++ CADASTRO02.ts
@@ -9,13 +9,13 @@ interface Aluno {
 }
 
 // os 5 alunos cadastrados
-const alunos: Aluno[] = [
-       { nome: "Carlos Eduardo", nota1: 8.0, nota2: 6.5, turma: "2A" },
-          { nome: "Fernanda Lima", nota1: 5.0, nota2: 4.5, turma: "2B" },
-       { nome: "Rafael Souza", nota1: 9.0, nota2: 8.5, turma: "2A" },
-   { nome: "Tatiane Moura", nota1: 6.0, nota2: 7.0, turma: "2C" },
-        { nome: "Lucas Pereira", nota1: 3.5, nota2: 5.0, turma: "2B" },
-]
+const alunos: Aluno[] = []
+frota.push      ( { nome: "Carlos Eduardo", nota1: 8.0, nota2: 6.5, turma: "2A" }),
+ frota.push       (  { nome: "Fernanda Lima", nota1: 5.0, nota2: 4.5, turma: "2B" }),
+ frota.push    (  { nome: "Rafael Souza", nota1: 9.0, nota2: 8.5, turma: "2A" }),
+frota.push  ( { nome: "Tatiane Moura", nota1: 6.0, nota2: 7.0, turma: "2C" }),
+ frota.push     (  { nome: "Lucas Pereira", nota1: 3.5, nota2: 5.0, turma: "2B" }),
+
 
 console.log("=== Situacao dos Alunos ===\n")
 
