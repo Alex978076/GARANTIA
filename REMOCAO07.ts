65ee54afdcc162b6b6b91fffe50b3b70bb7f1825
Alex978076 <joseantoniofilipealexandreviei@gmail.com>
2026-05-07 08:39:00 -0300

    Update REMOCAO07.ts

--- REMOCAO07.ts
+++ REMOCAO07.ts
@@ -12,14 +12,14 @@ interface produto {
 
 }
 // aqui eu coloquei os 6 produtos no array
-const produto: produto[] = [
-    { id: 1, nome: "Notebook Lenovo", preco: 2500.00, ativo: 21 },
-    { id: 2, nome: "Mouse sem fio", preco: 89.90, ativo: 22 },
-    { id: 3, nome: "Cadeira gamer", preco: 850.00, ativo: 23 },
-    { id: 4, nome: "Caneta BIC", preco: 3.50, ativo: 24 },
-    { id: 5, nome: "Monitor 24\"", preco: 1200.00, ativo: 25 },
-    { id: 6, nome: "Mochila", preco: 120.00, ativo: 26},
-]
+const produto: produto[] = []
+  frota.push ( { id: 1, nome: "Notebook Lenovo", preco: 2500.00, ativo: 21 }),
+  frota.push  ({ id: 2, nome: "Mouse sem fio", preco: 89.90, ativo: 22 }),
+  frota.push  ({ id: 3, nome: "Cadeira gamer", preco: 850.00, ativo: 23 }),
+  frota.push  ({ id: 4, nome: "Caneta BIC", preco: 3.50, ativo: 24 }),
+  frota.push  ({ id: 5, nome: "Monitor 24\"", preco: 1200.00, ativo: 25 }),
+  frota.push  ({ id: 6, nome: "Mochila", preco: 120.00, ativo: 26}),
+
 console.log("=== Produtos cadastrados ===")
 for (let i = 0; i < produtos.length; i++) {
   console.log(`[${produtos[i].id}] ${produtos[i].nome} - R$ ${produtos[i].preco.toFixed(2)}`)
